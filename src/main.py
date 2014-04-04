b11b9cb0be90b767d154d79a1a240a260f302b44
Allen Polak <PolakAllen@gmail.com>
2014-04-04 13:00:51 -0400

    Issues with macros, so redesigning

---
+++ src/main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import yaml
+import aptools.jinja.html as html
+
+def getdoc(filename):
+  with open(filename, 'r') as f:
+    return yaml.load(f)
+
+def main():
+  template = html.Templates("tmpl")
+  category = template["category"]
+  data = getdoc("data/raw.yaml")
+  with open("index.html","w") as f:
+    f.write(category.render(data))
+main()
